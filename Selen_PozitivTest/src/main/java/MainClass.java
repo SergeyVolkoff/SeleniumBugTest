4e131991ae2920ec7c48cbbb8cbeffa139b916cb
SergeyVolkoff <vol7745@yandex.ru>
2021-03-29 10:18:00 +0300

    v1

--- Selen_PozitivTest/src/main/java/MainClass.java
+++ Selen_PozitivTest/src/main/java/MainClass.java
@@ -12,16 +12,16 @@ public class MainClass {
         System.setProperty("webdriver.gecko.driver", "/Users/user/IdeaProjects/testselenium/drivers/geckodriver");
         System.setProperty("webdriver.chrome.driver", "/Users/user/IdeaProjects/testselenium/drivers/chromedriver");
         System.setProperty("phantomjs.binary.path", "/Users/user/IdeaProjects/testselenium/drivers/phantomjs-2.1.1-macosx/bin/phantomjs");
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();//будем работать в файрфоксе
         driver.manage();
-        driver.get("https://www.w3schools.com/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://www.w3schools.com/");// регистрацию будем проверять на этом сайте
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //добавим задержку
         WebElement buttonLogin = driver.findElement(By.xpath("//*[@id='w3loginbtn']"));
-        if (buttonLogin.getText().equals("LOG IN")){
-            System.out.println("Наименование кнопки LOG IN верное");
-            buttonLogin.click();
+        if (buttonLogin.getText().equals("LOG IN")){ 
+            System.out.println("Наименование кнопки LOG IN верное"); //проверяем что кнопка "наша"
+            buttonLogin.click(); //наша - кликаем
         }
-        else System.out.println("Имя кнопки LOG IN неверное");
+        else System.out.println("Имя кнопки LOG IN неверное"); //не наша - выводим оповещение в консоль
 
         WebElement buttonSignUp = driver.findElement(By.xpath("//div[@class='_yDlxv']//*[text()=' Sign up']"));
         if (buttonSignUp.getText().equals("Sign up")){
@@ -30,6 +30,14 @@ public class MainClass {
         }
         else System.out.println("Имя кнопки SignUp неверное");
 
-        
+        driver.findElement(By.xpath("//input[@id='modalusername']")).sendKeys("lovsql@mail.ru");//в поле емаил вводим наши валидные данные
+        driver.findElement(By.xpath("//input[@id='new-password']")).sendKeys("Qq123456");//в поле пароль вводимвалидный пароль
+        WebElement buttonSignUpFree= driver.findElement(By.xpath("//div[@class='_20LW8']//*[text()='Sign up for free']"));
+        if (buttonSignUpFree.getText().equals("Sign up for free")){ 
+            System.out.println("Наименование кнопки Sign up for free верное"); //проверяем что кнопка "наша"
+            buttonSignUpFree.click(); //наша - кликаем
+        }
+        else System.out.println("Имя кнопки Sign up for free  неверное"); 
+
     }
 }
